d628093a252f92be77ea2773b389c8ac2450140e
Mohammad Bahar <211053@ppu.edu.ps>
2025-05-01 20:14:38 +0300

    done

---
+++ lib/filter.dart
@@ -0,0 +1,246 @@
+import 'package:flutter/material.dart';
+
+enum Choices { choice1, choice2, choice3 }
+
+class Filter extends StatelessWidget {
+  const Filter({super.key});
+
+  Widget header() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 50.0, vertical: 20.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            'Filter',
+            style: TextStyle(fontSize: 40, color: Color(0x0ffbab9cf)),
+          ),
+          Icon(Icons.cancel, color: Color(0x0ffbab9cf)),
+        ],
+      ),
+    );
+  }
+
+  Widget searchBar() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 50.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            child: Text(
+              'Search by keyboard',
+              style: TextStyle(color: Color(0xff928eb6), fontSize: 30),
+            ),
+          ),
+          TextField(
+            decoration: InputDecoration(
+              fillColor: Color(0xff242046),
+              filled: true,
+              hintText: 'search jobs',
+              hintStyle: TextStyle(color: Color(0xff615e7d), fontSize: 22),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Color(0xff242046)),
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              prefixIcon: Icon(
+                Icons.search,
+                color: Color(0xff615e7d),
+                size: 38.0,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      resizeToAvoidBottomInset: false,
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Container(
+          decoration: BoxDecoration(
+            color: Color(0xFF322e59),
+            borderRadius: BorderRadius.circular(30.0),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              header(),
+              searchBar(),
+              Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 50.0,
+                  vertical: 20.0,
+                ),
+                child: Container(
+                  width: 300,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(
+                        child: Text(
+                          'VIEW',
+                          style: TextStyle(
+                            color: Color(0x0ff8b86ae),
+                            fontSize: 18,
+                          ),
+                        ),
+                      ),
+                      RadioExample(),
+                      SizedBox(height: 5.0),
+                      Text(
+                        'INCLUDE',
+                        style: TextStyle(
+                          color: Color(0x0ff8b86ae),
+                          fontSize: 20.0,
+                        ),
+                      ),
+                      CheckBoxI(),
+                      SizedBox(height: 80.0),
+                      Container(
+                        width: 400.0,
+                        decoration: BoxDecoration(
+                          color: Color(0xffffbc58),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Save changes ',
+                            style: TextStyle(fontSize: 20.0),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class RadioExample extends StatefulWidget {
+  const RadioExample({super.key});
+
+  @override
+  State<RadioExample> createState() => _RadioExampleState();
+}
+
+class _RadioExampleState extends State<RadioExample> {
+  Choices? _choice = Choices.choice1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Row(
+          children: [
+            Transform.scale(
+              scale: 2,
+              child: Radio<Choices>(
+                value: Choices.choice1,
+                groupValue: _choice,
+                activeColor: Colors.white70,
+                onChanged: (Choices? value) {
+                  setState(() {
+                    _choice = value;
+                  });
+                },
+              ),
+            ),
+            SizedBox(width: 15),
+            Text(
+              'All jobs',
+              style: TextStyle(color: Color(0x0ff8b86ae), fontSize: 20.0),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            Transform.scale(
+              scale: 2,
+              child: Radio<Choices>(
+                value: Choices.choice2,
+                groupValue: _choice,
+                activeColor: Colors.white70,
+                onChanged: (Choices? value) {
+                  setState(() {
+                    _choice = value;
+                  });
+                },
+              ),
+            ),
+            SizedBox(width: 15),
+            Text(
+              'Active job posts',
+              style: TextStyle(color: Color(0x0ff8b86ae), fontSize: 20.0),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            Transform.scale(
+              scale: 2,
+              child: Radio<Choices>(
+                value: Choices.choice3,
+                groupValue: _choice,
+                activeColor: Colors.white70,
+                onChanged: (Choices? value) {
+                  setState(() {
+                    _choice = value;
+                  });
+                },
+              ),
+            ),
+            SizedBox(width: 15),
+            Text(
+              'All active job posts',
+              style: TextStyle(color: Color(0x0ff8b86ae), fontSize: 20),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
+
+class CheckBoxI extends StatefulWidget {
+  const CheckBoxI({super.key});
+
+  @override
+  State<CheckBoxI> createState() => _CheckBoxIState();
+}
+
+class _CheckBoxIState extends State<CheckBoxI> {
+  bool isChecked = false;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Checkbox(
+          value: isChecked,
+          onChanged: (bool? value) {
+            setState(() {
+              isChecked = value!;
+            });
+          },
+        ),
+        Text(
+          'Jobs shared with me',
+          style: TextStyle(color: Color(0x0ff8b86ae), fontSize: 20.0),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/plan.dart
@@ -0,0 +1,281 @@
+import 'package:flutter/material.dart';
+
+class Plan extends StatefulWidget {
+  const Plan({super.key});
+
+  @override
+  State<Plan> createState() => _PlanState();
+}
+
+class _PlanState extends State<Plan> {
+  bool isChecked1 = false;
+  bool isChecked2 = false;
+  Widget header() {
+    return Column(
+      children: [
+        Text(
+          'Try FREE for 4 weeks',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          'We uncover the facts around the',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        Text(
+          'clock, all over the globe.',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 16,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget buy() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Column(
+        children: <Widget>[
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                isChecked1 = true;
+                isChecked2 = false;
+              });
+            },
+            child: Container(
+              height: 140,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                border: Border.all(
+                  color:
+                      isChecked1 == true
+                          ? Color(0xff5b6acd)
+                          : Color(0xffebeaf9),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: 15.0,
+                  horizontal: 5.0,
+                ),
+                child: Row(
+                  children: [
+                    Transform.scale(
+                      scale: 2,
+                      child: Checkbox(
+                        value: isChecked1,
+                        onChanged: (bool? value) {
+                          setState(() {
+                            isChecked1 = value!;
+                            if (isChecked1 == true) {
+                              isChecked2 = false;
+                            }
+                          });
+                        },
+                      ),
+                    ),
+                    SizedBox(width: 5),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Monthly',
+                          style: TextStyle(color: Colors.black, fontSize: 20.0),
+                        ),
+                        SizedBox(height: 10.0),
+                        Text(
+                          '4 weeks for free',
+                          style: TextStyle(color: Colors.black, fontSize: 20.0),
+                        ),
+                        SizedBox(height: 10.0),
+                        Text(
+                          'Then 3 every month for the first year',
+                          style: TextStyle(color: Colors.black, fontSize: 16.0),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 25.0),
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                isChecked1 = false;
+                isChecked2 = true;
+              });
+            },
+            child: Container(
+              height: 170,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                border: Border.all(
+                  color:
+                      isChecked2 == true
+                          ? Color(0xff5b6acd)
+                          : Color(0xffebeaf9),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 10.0,
+                  vertical: 15.0,
+                ),
+                child: Row(
+                  children: [
+                    Transform.scale(
+                      scale: 2,
+                      child: Checkbox(
+                        value: isChecked2,
+                        onChanged: (bool? value) {
+                          setState(() {
+                            isChecked2 = value!;
+                            if (isChecked2 == true) {
+                              isChecked1 = false;
+                            }
+                          });
+                        },
+                      ),
+                    ),
+                    SizedBox(width: 15),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          padding: EdgeInsets.symmetric(
+                            horizontal: 8,
+                            vertical: 4,
+                          ),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20.0),
+                            color: Color(0xff3243db),
+                          ),
+                          child: Text(
+                            'BEST VALUE',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 6.0),
+                        Text(
+                          'Yearly',
+                          style: TextStyle(color: Colors.black, fontSize: 20.0),
+                        ),
+                        SizedBox(height: 10.0),
+                        Text(
+                          '4 weeks for free',
+                          style: TextStyle(color: Colors.black, fontSize: 20.0),
+                        ),
+                        SizedBox(height: 10.0),
+                        Text(
+                          'Then 30 for the first year',
+                          style: TextStyle(color: Colors.black, fontSize: 16.0),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget continueButton() {
+    return Column(
+      children: [
+        Container(
+          width: 300.0,
+          height: 50.0,
+          decoration: BoxDecoration(
+            color: Colors.black,
+            borderRadius: BorderRadius.circular(50.0),
+          ),
+          child: TextButton(
+            onPressed: () {},
+            child: Text(
+              'Continue ',
+              style: TextStyle(fontSize: 20.0, color: Colors.white),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget continuePayPal() {
+    return Column(
+      children: [
+        Container(
+          width: 300.0,
+          height: 50.0,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(50.0),
+            border: Border.all(color: Color(0xfff5f6f9)),
+          ),
+          child: TextButton(
+            onPressed: () {},
+            child: Text(
+              'Continue with PayPal ',
+              style: TextStyle(fontSize: 20.0, color: Colors.black),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(40.0),
+              color: Colors.white,
+            ),
+            width: 600.0,
+            child: Column(
+              children: [
+                header(),
+                SizedBox(height: 20.0),
+                buy(),
+                SizedBox(height: 40.0),
+                continueButton(),
+                SizedBox(height: 10.0),
+                continuePayPal(),
+                TextButton(
+                  onPressed: () {},
+                  child: Text(
+                    'View more offers',
+                    style: TextStyle(color: Colors.blue),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
